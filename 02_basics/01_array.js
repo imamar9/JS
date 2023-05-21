3373c9988a20f613408740f3970c605c123f1be8
Amar Shukla <34983235+imamar9@users.noreply.github.com>
2023-05-21 09:57:29 +0000

    array basics

---
+++ 02_basics/01_array.js
@@ -0,0 +1,15 @@
+
+const front_end = ["HTML", "CSS", "Javascript"]
+const back_end = ["Java", "Node", "Python"]
+
+// const new_arr = front_end.push(back_end)
+
+// console.log(new_arr)
+
+// console.log(front_end.concat(back_end))
+
+const all_tech = [...front_end, ...back_end]
+
+console.log(all_tech)
+
+console.log(Array.from("Amar"))
