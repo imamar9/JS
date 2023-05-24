ebe8f6f5564defa08e7223c409807cb0b46143a2
Amar Shukla <34983235+imamar9@users.noreply.github.com>
2023-05-24 16:38:47 +0000

    basic functions

---
+++ 02_basics/04_functions.js
@@ -0,0 +1,31 @@
+// function expression or function declaration
+function add(a, b) {
+    return a + b;
+}
+
+console.log(add(5,6));
+
+// function expression
+
+let sub = function(a, b) {
+    return a - b;
+}
+
+console.log(sub(20, 13));
+
+// named function expression
+
+let mul = function multiply(x, y) {
+    return x * y;
+}
+
+console.log(mul(5, 6));
+
+// arrow function
+
+let div = (x, y) => {
+    return x / y;
+}
+
+console.log(30/10);
+ 
